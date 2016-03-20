f0af2dd8bbb2e8e9b1b5407bb204143bb7fabdee
Bas Meijer <bas.meijer@me.com>
2016-03-20 22:15:04 +0100

    fix for #6

---
+++ templates/deploy-file.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#https://github.com/JFrogDev/project-examples PR11
+
+usage() {
+    echo "Deploy a local file to Artifactory keeping the same file name"
+    echo "Usage: $0 localFilePath targetFolder"
+    exit 1
+}
+
+if [ -z "$2" ]; then
+    usage
+fi
+
+if [ -z "$ARTIFACTORY_USER" ]; then
+    echo "Ensure you have the environment variables set"
+    echo "ARTIFACTORY_USER"
+    exit 1
+fi
+
+if [ -z "$ARTIFACTORY_PASSWORD" ]; then
+    echo "Ensure you have the environment variables set"
+    echo "ARTIFACTORY_PASSWORD"
+    exit 1
+fi
+localFilePath="$1"
+targetFolder="$2"
+
+if [ ! -f "$localFilePath" ]; then
+    echo "ERROR: local file $localFilePath does not exists!"
+    exit 1
+fi
+
+
+which openssl 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Cannot find openssl"
+  exit 1
+fi
+
+echo "Computing checksums"
+md5Value="`openssl dgst -md5 "$localFilePath" | cut -d= -f2 |tr -d [:space:]]`"
+md5Value="${md5Value:0:32}"
+sha1Value="`openssl dgst -sha1 "$localFilePath" | cut -d= -f2 |tr -d [:space:]]`"
+sha1Value="${sha1Value:0:40}"
+fileName="`basename "$localFilePath"`"
+
+echo $md5Value $sha1Value $localFilePath
+
+echo "INFO: Uploading $localFilePath to $targetFolder/$fileName"
+curl -i -X PUT -u $ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD \
+ -H "X-Checksum-Md5: $md5Value" \
+ -H "X-Checksum-Sha1: $sha1Value" \
+ -T "$localFilePath" \
+ "$targetFolder/$fileName"
+
